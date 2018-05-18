4be2e76aa62881cce457bd811af1038555ba2111
Ruochen Lyu <kshift.lv@gmail.com>
2018-05-18 22:34:01 +0800

    modify style

--- main.js
+++ main.js
@@ -4,9 +4,27 @@ request.open('GET', 'https://api.unsplash.com/photos/random?client_id=c45833dd0f
 request.onload = function() {
   if (request.status >= 200 && request.status < 400) {
     var data = JSON.parse(request.responseText);
-    document.body.style.backgroundImage = `url(${data.urls.regular})`;
-    document.body.insertAdjacentHTML('beforeend', `<div class="source">Photo by <a href="${data.links.html}" target="_blank">${data.user.name}</a></div>`)
+  } else {
+    var data = {
+      urls: {
+        full: 'https://images.unsplash.com/photo-1501979735065-dfe1580b4a49?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9f5d06a687d9ac4cf9e384f02e03e3bc&auto=format&fit=crop&w=1650&q=80',
+      },
+      links: {
+        html: 'https://unsplash.com/photos/nK01_Z9dKqI'
+      },
+      user: {
+        name: 'Michael Brandt'
+      }
+    }
   }
+
+  var image = new Image();
+  image.onload = function () {
+    document.body.style.backgroundImage = `url(${data.urls.full})`;
+    document.body.querySelector('main').insertAdjacentHTML('beforeend', `<div class="source">Photo by <a href="${data.links.html}" target="_blank">${data.user.name}</a></div>`)
+  }
+
+  image.src = data.urls.full;
 };
 
 request.send();
